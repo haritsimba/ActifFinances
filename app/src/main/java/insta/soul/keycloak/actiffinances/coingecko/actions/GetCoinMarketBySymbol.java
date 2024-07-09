625a7d12ba5b92ae71b611966a249363a69dcec6
haritsimba <haritsimbafranck@gmail.com>
2024-07-09 12:54:41 +0300

    GetCoinMarketBySymbol

--- app/src/main/java/insta/soul/keycloak/actiffinances/coingecko/actions/GetCoinMarketBySymbol.java
+++ app/src/main/java/insta/soul/keycloak/actiffinances/coingecko/actions/GetCoinMarketBySymbol.java
@@ -18,12 +18,6 @@ import retrofit2.Response;
 public class GetCoinMarketBySymbol {
     private String symbol;
 
-    public GetCoinMarketBySymbol(String symbol, CoingeckoApiServices services, GetCoinMarketBySymbolCallback callback) {
-        this.symbol = symbol;
-        this.services = services;
-        this.callback = callback;
-    }
-
     private CoingeckoApiServices services;
     private GetCoinMarketBySymbolCallback callback;
 
